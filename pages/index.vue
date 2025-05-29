2bf4008146ebf04522e7f72d1e43dc0d0978d249
jubonnu <136735971+jubonnu@users.noreply.github.com>
2025-05-29 23:10:44 +0900

    Fix index page redirection

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+// Redirect to login page
+const router = useRouter();
+
+// Only redirect on client-side
+onMounted(() => {
+  router.push('/login');
+});
+</script>
+
 <template>
-  <div></div>
+  <div class="min-h-screen flex items-center justify-center">
+    <el-spinner />
+  </div>
 </template>
