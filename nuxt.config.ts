cf7f8b157eaf452a7ba9bb9e2d400dbd08bbf91b
jubonnu <136735971+jubonnu@users.noreply.github.com>
2025-05-30 00:04:18 +0900

    Fix Nuxt server configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  devServer: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
