104698ab39b989e5efab4f7dff5c7d1840073484
jubonnu <136735971+jubonnu@users.noreply.github.com>
2025-06-02 02:42:33 +0900

    Fix duplicate attributes in Instagram page

--- pages/admin/instagram.vue
+++ pages/admin/instagram.vue
@@ -1,10 +1,12 @@
-@@ .. @@
-     <div 
--      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
-       v-loading="loading"
--      v-if="sortedPosts.length > 0"
-+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
-+      v-show="sortedPosts.length > 0"
-     >
-       <div 
-         v-for="post in sortedPosts"
+<div 
+  v-loading="loading"
+  class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+  v-show="sortedPosts.length > 0"
+>
+  <div 
+    v-for="post in sortedPosts"
+    :key="post.id"
+  >
+    <PostCard :post="post" />
+  </div>
+</div>
